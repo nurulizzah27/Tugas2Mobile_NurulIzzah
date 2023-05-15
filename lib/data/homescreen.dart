1a26d11316bc81160540dc5f629688cd26f77078
Nurul Izzah <nurulizzah090319@gmail.com>
2023-05-15 19:05:06 +0800

    first commit

---
+++ lib/data/homescreen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:cooking/data/dataresep.dart';
+import 'package:cooking/data/resep.dart';
+
+class home extends StatelessWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daftar Resep Makanan'),
+        backgroundColor:Color.fromARGB(255, 231, 168, 58),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+          itemCount: dataResep.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DetailScreen(
+                              reseps: dataResep[index],
+                            )));
+              },
+              child: Card(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Image.asset(
+                          dataResep[index].image,
+                          width: 150,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(dataResep[index].name,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                                color: Color.fromARGB(255, 231, 168, 58))),
+                        Text('PRICE : ${dataResep[index].htm}'),
+                        ClipOval(
+                          child: Material(
+                            color: Colors.blue, // button color
+                            child: InkWell(
+                              splashColor: Colors.red, // inkwell color
+                              child: SizedBox(
+                                  width: 30,
+                                  height: 30,
+                                  child: Icon(Icons.favorite)),
+                              onTap: () {
+                              },
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
